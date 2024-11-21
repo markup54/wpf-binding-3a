c9b52f0a1a1ba1dde4f80f62563c93cc7f24cbe5
markup54 <marzenakupka@tlen.pl>
2024-11-21 08:55:21 +0100

    Dodaj pliki projektów.

---
+++ wpf binding 3a/Window2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf_binding_3a
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("chcesz obliczyć pole prostokąta?", "Pytanie", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+    }
+}
